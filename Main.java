88798a9ab8588c33b566e0b43db36236be2ad94e
UlianaDi <99318121+UlianaDi@users.noreply.github.com>
2023-01-31 11:21:27 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+
+    public class Main {
+        public static void main(String[] args) {
+            One one = new One();
+            one.setA(1);
+            one.setB(2);
+            one.setC(3);
+
+            System.out.println(one.getA());
+            System.out.println(one.getB());
+            System.out.println(one.getC());
+        }
+    }
+
